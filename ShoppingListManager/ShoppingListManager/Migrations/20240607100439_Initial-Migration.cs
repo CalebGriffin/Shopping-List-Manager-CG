2886b92df5ff6d164dbde000bc8a68d5d8269de3
Caleb Griffin <calebegriffin@gmail.com>
2024-06-07 11:06:26 +0100

    Updated DbContext Settings and Added Initial Migration

---
+++ ShoppingListManager/ShoppingListManager/Migrations/20240607100439_Initial-Migration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingListManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    IsImportant = table.Column<bool>(type: "bit", nullable: false),
+                    SortOrder = table.Column<int>(type: "int", nullable: false),
+                    ListType = table.Column<int>(type: "int", nullable: false),
+                    CurrentSortMode = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
--- ShoppingListManager/ShoppingListManager/Program.cs
+++ ShoppingListManager/ShoppingListManager/Program.cs
@@ -1,8 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingListManager.Data;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SLMDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SLMDbContext"));
+});
 
 var app = builder.Build();
 
